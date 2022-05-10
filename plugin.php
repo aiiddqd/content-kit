f3bd1b8fa1ccf7f5d72964cc4d19c8b0c34bb7b5
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-05-10 12:55:17 +0300

    small updates

--- plugin.php
+++ plugin.php
@@ -1,21 +1,30 @@
 <?php
 /*
-* Plugin Name: @ Content Kit by @uptimizt
-* Description: Lazy Blocks
+* Plugin Name: _ Content Kit Base
+* Description: Block based design system with additional styles and patterns
 * Author: uptimizt
-* GitHub Plugin URI: https://github.com/uptimizt/content-kit-u7
-* Version: 0.20220407
+* GitHub Plugin URI: https://github.com/uptimizt/content-kit-base
+* Version: 0.220510
 */
 
 namespace uptimizt\LazyBlocks;
 
-add_action('plugins_loaded', function () {
-  add_filter('lzb/block_render/include_template', __NAMESPACE__ . '\\' . 'chg_template_path', 10, 4);
-  add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\' . 'frontend');
-  add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\\' . 'backend');
-  add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\' . 'commone_style');
-  blocks_load_configs();
-});
+add_filter('lzb/block_render/include_template', __NAMESPACE__ . '\\' . 'chg_template_path', 10, 4);
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\' . 'frontend');
+add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\\' . 'backend');
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\' . 'commone_style');
+add_action('plugins_loaded', __NAMESPACE__ . '\\blocks_load_configs');
+add_action('plugins_loaded', __NAMESPACE__ . '\\styles_load_configs');
+
+
+
+function styles_load_configs()
+{
+  $files = glob(__DIR__ . '/styles/*/config.php');
+  foreach ($files as $file) {
+    require_once $file;
+  }
+}
 
 
 function blocks_load_configs()
