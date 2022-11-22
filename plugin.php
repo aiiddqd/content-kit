9dc147a9a4b40cb2988d74a5a23191183c518063
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-11-22 18:02:22 +0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 * Description: Block based design system with additional styles and patterns
 * Author: uptimizt
 * GitHub Plugin URI: https://github.com/uptimizt/content-kit
-* Version: 0.220510
+* Version: 0.221122
 */
 
 namespace ContentKitBase;
