60ffb761b9e98197bbe9880b7c63d4a76bfd640f
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-12-12 20:47:27 +0300

    draft for lists mod and for media text pattern

---
+++ patterns/media-and-text-1.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Title: Media and text 1
+ * Slug: content-kit/media-and-text-1
+ * Categories: text
+ */
+?>
+<!-- wp:media-text {"mediaPosition":"right","mediaId":25087,"mediaLink":"https://bizzapps.ru/test/800x0/","mediaType":"image","mediaWidth":40,"className":"is-style-default"} -->
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-style-default"
+    style="grid-template-columns:auto 40%">
+    <div class="wp-block-media-text__content">
+        <!-- wp:paragraph {"placeholder":"Content…","className":"is-style-above-header","fontSize":"small"} -->
+        <p class="is-style-above-header has-small-font-size"><strong>Above header</strong></p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:heading -->
+        <h2>Neque porro quisquam est qui dolorem ipsum quia dolor</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:list {"className":"is-style-default"} -->
+        <ul class="is-style-default">
+            <!-- wp:list-item -->
+            <li>Lorem Ipsum has been the industry's</li>
+            <!-- /wp:list-item -->
+
+            <!-- wp:list-item -->
+            <li>It is a long established fact that a reader will be distracted by the readable content of a page when
+                looking at its layout</li>
+            <!-- /wp:list-item -->
+
+            <!-- wp:list-item -->
+            <li>Many desktop publishing packages and web page</li>
+            <!-- /wp:list-item -->
+        </ul>
+        <!-- /wp:list -->
+
+        <!-- wp:buttons -->
+        <div class="wp-block-buttons">
+            <!-- wp:button {"backgroundColor":"primary"} -->
+            <div class="wp-block-button"><a
+                    class="wp-block-button__link has-primary-background-color has-background wp-element-button">More</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+    </div>
+    <figure class="wp-block-media-text__media"><img
+            src="https://bizzapps.ru/wp-content/uploads/sites/24/2022/12/800x0.webp" alt=""
+            class="wp-image-25087 size-full" /></figure>
+</div>
+<!-- /wp:media-text -->
