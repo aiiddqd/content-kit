0a90035822a2a8f74f9ee4f607c2dfafec1228e3
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-12-24 14:01:55 +0300

    add language domain

--- plugin.php
+++ plugin.php
@@ -1,14 +1,22 @@
 <?php
 /*
-* Plugin Name: @ ContentKit
-* Description: Block based design system with additional styles and patterns
-* Author: uptimizt
-* GitHub Plugin URI: https://github.com/uptimizt/content-kit
-* Version: 0.221217
-*/
+ * Plugin Name: @ ContentKit
+ * Description: Block based design system with additional styles and patterns
+ * Author: uptimizt
+ * GitHub Plugin URI: https://github.com/uptimizt/content-kit
+ * Text Domain: content-kit
+ * Domain Path: /languages
+ * Version: 0.221223
+ */
 
 namespace ContentKit;
 
+defined('ABSPATH') || die;
+
 require_once __DIR__ . '/mods/loader.php';
 require_once __DIR__ . '/patterns/_loader.php';
-require_once(__DIR__ . '/includes/ColorsTypical.php');
+require_once __DIR__ . '/includes/ColorsTypical.php';
+
+add_action('plugins_loaded', function () {
+    load_plugin_textdomain('content-kit', false, __DIR__ . '/languages/');
+});
