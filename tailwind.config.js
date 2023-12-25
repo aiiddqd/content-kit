e482e0432d1533041c58ad3382163ba8aff078bf
Anatolii Iumashev <a.yumashev@gmail.com>
2023-12-25 12:58:42 +0300

    save

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  corePlugins: ['float', 'padding', 'margin'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
